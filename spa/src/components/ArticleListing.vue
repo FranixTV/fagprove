39af075ebe56b9191de01682ca63274a372d6d75
Rikart Svendsgård <rikart@seria.no>
2021-07-29 17:25:45 +0200

    Added filter to sort by article age

--- spa/src/components/ArticleListing.vue
+++ spa/src/components/ArticleListing.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="article-list-outer">
-    <input class="article-search" type="search" name="freeSearch" placeholder="Frisøk..." v-model="textSearch"/>
-    <div class="article-list" v-if="listArticles">
+    <div class="filters-container">
+      <input aria-label="Frisøk på artikler" class="article-search" type="search" name="freeSearch" placeholder="Frisøk..." v-model="textSearch"/>
+      <select aria-label="Sorter artikler basert på alder" name="ageFilter" class="article-age-filter" v-model="ageFilter">
+        <option value="DESC">Nyest - Eldst</option>
+        <option value="ASC">Eldst - Nyest</option>
+      </select>
+    </div>
+    <div class="article-list" v-if="articles">
       <template v-for="article of listArticles" :key="article.id">
         <Article :article="article" @readmore="toggleReadmore"/>
       </template>
@@ -20,16 +26,23 @@ export default {
   props: ["articles"],
   data() {
       return {
-        textSearch: ""
+        textSearch: "",
+        ageFilter: "DESC"
       }
   },
   computed: {
     listArticles: function () {
-      if(!this.textSearch) {
-        return this.articles;
+      let showArticles = this.articles;
+      if(this.textSearch) {
+        let filter = article => article.title.toLowerCase().includes(this.textSearch.toLowerCase()) || (article.summary && article.summary.toLowerCase().includes(this.textSearch.toLowerCase())) || (article.content && article.content.toLowerCase().includes(this.textSearch.toLowerCase()));
+        showArticles = showArticles.filter(filter);
       }
-      let filter = article => article.title.toLowerCase().includes(this.textSearch.toLowerCase()) || (article.summary && article.summary.toLowerCase().includes(this.textSearch.toLowerCase())) || (article.content && article.content.toLowerCase().includes(this.textSearch.toLowerCase()));
-      return this.articles.filter(filter);
+      if(this.ageFilter === "ASC") {
+        showArticles = showArticles.sort((article1, article2) => article1.articleid - article2.articleid)
+      } else {
+        showArticles = showArticles.sort((article1, article2) => article2.articleid - article1.articleid)
+      }
+      return showArticles;
     }
   },
   methods: {
